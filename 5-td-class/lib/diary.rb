ef74b87986903ab3729588a56be0ccdf57aacf43
Sean Peters <sp@snptrs.com>
2023-05-12 13:16:00 +0100

    Completes tdd a class exercise

--- 5-td-class/lib/diary.rb
+++ 5-td-class/lib/diary.rb
@@ -1,37 +1,40 @@
 class DiaryEntry
   def initialize(title, contents) #title, contents are strings
-
+    @title = title
+    @contents = contents
+    @next_chunk_start = 0
   end
 
   def title
-    # Returns title as a string
+    return @title
   end
 
   def contents
-    # Returns the contents of a string
+    return @contents
   end
 
   def count_words
-    # Returns the number of words in the contents as an integer
+    return words.count
   end
 
   def reading_time(wpm)
-    # wpm is an integer representing the number of words the
-    # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+    fail "Reading speed must be greater than 0." unless wpm > 0
+    return (count_words / wpm.to_f).ceil
   end
 
   def reading_chunk(wpm, minutes)
-    # `wpm` is an integer representing the number
-    # of words the user can read per minute
-    # `minutes` is an integer representing the
-    # number of minutes the user has to read
-    # Returns a string with a chunk of the contents that the user could read
-    # in the given number of minutes.
-    # If called again, `reading_chunk` should return the next chunk, skipping
-    # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
+    fail "Reading speed must be greater than 0." unless wpm > 0
+    words_we_can_read = wpm * minutes
+    @next_chunk_start = 0 if @next_chunk_start >= words.length
+    chunk = words[@next_chunk_start, words_we_can_read].join(" ").strip
+    @next_chunk_start += words_we_can_read
+    return chunk
+  end
+
+  private
+
+  def words
+    @contents.split(" ")
   end
 
 end
--- 5-td-class/spec/diary_spec.rb
+++ 5-td-class/spec/diary_spec.rb
@@ -1,2 +1,78 @@
 require 'diary'
 
+RSpec.describe DiaryEntry do
+  context "when initialized" do
+    it "accepts a title and contents" do
+      entry = DiaryEntry.new("Monday", "Lorem ipsum dolor sit amet.")
+    end
+  end
+
+  context "`title` method" do
+    it "returns the entry title as a string" do
+      entry = DiaryEntry.new("Monday", "Lorem ipsum dolor sit amet.")
+      expect(entry.title).to eq "Monday"
+    end
+  end
+
+  context "`contents` method" do
+    it "returns the entry contents as a string" do
+      entry = DiaryEntry.new("Monday", "Lorem ipsum dolor sit amet.")
+      expect(entry.contents).to eq "Lorem ipsum dolor sit amet."
+    end
+  end
+
+  context "`count_words` method" do
+    it "returns the word count for the entry contents as an integer" do
+      entry = DiaryEntry.new("Monday", "Lorem ipsum dolor sit amet.")
+      expect(entry.count_words).to eq 5
+    end
+  end
+
+  context "`reading_time` method returns an integer in wpm" do
+    it "returns 1 for a wpm value of 200 and contents of 200 words" do
+      entry = DiaryEntry.new("Monday", "Lorem " * 200)
+      expect(entry.reading_time(200)).to eq 1
+    end
+    it "returns 1 for a wpm value of 200 and contents of 100 words" do
+      entry = DiaryEntry.new("Monday", "Lorem " * 100)
+      expect(entry.reading_time(200)).to eq 1
+    end
+    it "returns 2 for a wpm value of 200 and contents of 400 words" do
+      entry = DiaryEntry.new("Monday", "Lorem " * 400)
+      expect(entry.reading_time(200)).to eq 2
+    end
+    it "fails for a reading speed of 0" do
+      entry = DiaryEntry.new("Monday", "Lorem " * 400)
+      expect { entry.reading_time(0) }.to raise_error "Reading speed must be greater than 0."
+    end
+  end
+
+  describe "reading_chunk" do
+    context "with a contents readable within the given minutes" do
+      it "returns the full text" do
+        entry = DiaryEntry.new("Monday", "Lorem " * 100)
+        expect(entry.reading_chunk(200, 1)).to eq ("Lorem " * 100).strip
+      end
+    end
+    context "with a contents not readable within the given minutes" do
+      it "returns the first section of the text when called the first time" do
+        entry = DiaryEntry.new("Monday", (("Lorem " * 100) + ("Ipsum " * 100)))
+        expect(entry.reading_chunk(100, 1)).to eq ("Lorem " * 100).strip
+      end
+
+      it "returns the second section of the text when called the second time, etc." do
+        entry = DiaryEntry.new("Monday", (("Lorem " * 200) + ("Ipsum " * 200) + ("Dolor " * 200)))
+        expect(entry.reading_chunk(200, 1)).to eq ("Lorem " * 200).strip
+        expect(entry.reading_chunk(200, 1)).to eq ("Ipsum " * 200).strip
+        expect(entry.reading_chunk(200, 1)).to eq ("Dolor " * 200).strip
+      end
+
+      it "restarts when it reaches the end" do
+        entry = DiaryEntry.new("Monday", (("Lorem " * 200) + ("Ipsum " * 200)))
+        expect(entry.reading_chunk(200, 1)).to eq ("Lorem " * 200).strip
+        expect(entry.reading_chunk(200, 1)).to eq ("Ipsum " * 200).strip
+        expect(entry.reading_chunk(100, 2)).to eq ("Lorem " * 200).strip
+      end
+    end
+  end
+end
