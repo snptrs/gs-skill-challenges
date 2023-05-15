f83728f6c3061b6975414b19f2290f8c854722c4
Sean Peters <sp@snptrs.com>
2023-05-15 11:37:49 +0100

    Fixes LetterCounter

--- 7-debugging-2/lib/letter_counter.rb
+++ 7-debugging-2/lib/letter_counter.rb
@@ -4,15 +4,15 @@ class LetterCounter
   end
 
   def calculate_most_common()
-    counter = Hash.new(1)
+    counter = Hash.new
     most_common = nil
-    most_common_count = 1
+    most_common_count = 0
     @text.chars.each do |char|
       next unless is_letter?(char)
-      counter[char] = (counter[char] || 1) + 1
+      counter[char] = (counter[char] || 0) + 1
       if counter[char] > most_common_count
         most_common = char
-        most_common_count += counter[char]
+        most_common_count = counter[char]
       end
     end
     return [most_common_count, most_common]
