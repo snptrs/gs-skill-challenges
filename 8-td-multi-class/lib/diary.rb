a7a37fa931d297d5481f36e4d2730177eeb18e95
Sean Peters <sean@snptrs.dev>
2023-05-17 12:50:24 +0100

    Tidy up diary

--- 8-td-multi-class/lib/diary.rb
+++ 8-td-multi-class/lib/diary.rb
@@ -19,22 +19,12 @@ class Diary
     return total
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # if the user were to read all entries in the diary.
+  def reading_time(wpm)
     fail "Error: wpm should be a non-negative integer" if wpm < 0
     (count_words / wpm.to_f).ceil
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
-        # `wpm` is an integer representing the number of words the user can read
-        # per minute.
-        # `minutes` is an integer representing the number of minutes the user
-        # has to read.
-    # Returns an instance of diary entry representing the entry that is closest 
-    # to, but not over, the length that the user could read in the minutes they
-    # have available given their reading speed.
     readable_entries = @diary.filter do |entry|
       entry.reading_time(wpm) <= minutes
     end
